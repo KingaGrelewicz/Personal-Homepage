c3eca4514058f37ca4eb61fd494e1ade2f9fee6f
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2024-02-04 14:44:25 +0100

    Skills and WantToLearn Components created

---
+++ src/components/Section/WantToLearn/index.js
@@ -0,0 +1,14 @@
+import { SectionList, SectionListItem } from "../styled";
+
+const WantToLearn = () => {
+    return (
+        <SectionList>
+                <SectionListItem>Next.js</SectionListItem>
+                <SectionListItem>Vite</SectionListItem>
+                <SectionListItem>CSS Emotion</SectionListItem>
+                <SectionListItem>Unit testing</SectionListItem>
+        </SectionList>
+    )
+}
+
+export default WantToLearn;
