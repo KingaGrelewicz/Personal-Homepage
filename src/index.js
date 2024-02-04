f89f4d7569a6ef888d7c4735990f775ff101f997
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2024-02-04 14:42:46 +0100

    Theme provider and LightTheme added into index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from 'styled-components';
+import { LightTheme } from './theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ThemeProvider theme={LightTheme}>
     <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
