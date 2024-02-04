943153edbe4ec9f1b801cca13271fae871449415
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2024-02-04 17:18:41 +0100

    Footer updated

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,17 @@
-import { FooterMail, FooterWrapper } from "./styled";
+import { FooterContent, FooterMail, FooterParagraph, FooterWrapper } from "./styled";
 
 
 const Footer = () => {
     return (
         <FooterWrapper>
-            <p>Let's Talk</p>
-            <FooterMail href="mailto:kinga.grelewicz@gmail.com">kinga.grelewicz@gmail.com</FooterMail>
+            <FooterParagraph>LET'S TALK</FooterParagraph>
+            <FooterMail 
+                href="mailto:kinga.grelewicz@gmail.com">kinga.grelewicz@gmail.com
+            </FooterMail>
+            <FooterContent>
+                I am always open to new challenges.<br /> 
+                If you need help implementing your ideas, feel free to contact me 👋
+            </FooterContent>
         </FooterWrapper>
     )
 };
--- src/components/Footer/styled.js
+++ src/components/Footer/styled.js
@@ -2,8 +2,21 @@ import styled from "styled-components";
 
 export const FooterWrapper = styled.div``;
 
+export const FooterParagraph = styled.p`
+    color: ${({ theme }) => theme.color.slateGray};
+    font-size: 12px;
+    font-weight: 700;
+`;
+
 export const FooterMail = styled.span`
     color: ${({ theme }) => theme.color.mineShaft};
     font-size: 32px;
     font-weight: 900;
+    margin: 24px 0;
+`;
+
+export const FooterContent = styled.p`
+    color: ${({ theme }) => theme.color.mineShaft};
+    font-size: 18px;
+    font-weight: 400;
 `;
