9015cc9fd016aea15ec2b2ba8957df3ad30b626e
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2024-02-04 14:33:07 +0100

    LightTheme created

---
+++ src/theme.js
@@ -0,0 +1,10 @@
+export const LightTheme = {
+    color: {
+        white: "#FFFFFF",
+        whiteLilac: "#FBFBFE",
+        iron: "#D1D5DA",
+        slateGray: "#6E7E91",
+        scienceBlue: "#0366D6",
+        mineShaft: "#252525"
+    }
+};
