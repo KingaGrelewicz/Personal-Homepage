582e7f24bd8da3d8419c0e81ddb0393526305a81
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2024-02-05 23:24:20 +0100

    SectionList corrected

--- src/components/Section/Skills/index.js
+++ src/components/Section/Skills/index.js
@@ -4,17 +4,17 @@ import { SectionList, SectionListItem } from "../styled";
 const Skills = () => {
     return (
         <SectionList>
-                <SectionListItem>HTML</SectionListItem>
-                <SectionListItem>CSS Flex</SectionListItem>
-                <SectionListItem>CSS Grid</SectionListItem>
-                <SectionListItem>Styled Components</SectionListItem>
-                <SectionListItem>CSS BEM Convention</SectionListItem>
-                <SectionListItem>React Router</SectionListItem>
-                <SectionListItem>React Hooks</SectionListItem>
-                <SectionListItem>Error handling</SectionListItem>
-                <SectionListItem>Working with API (fetch, axios)</SectionListItem>
-                <SectionListItem>Promises (asyns, await)</SectionListItem>
-                <SectionListItem>Trello</SectionListItem>
+            <SectionListItem>HTML</SectionListItem>
+            <SectionListItem>CSS Flex</SectionListItem>
+            <SectionListItem>CSS Grid</SectionListItem>
+            <SectionListItem>Styled Components</SectionListItem>
+            <SectionListItem>CSS BEM Convention</SectionListItem>
+            <SectionListItem>React Router</SectionListItem>
+            <SectionListItem>React Hooks</SectionListItem>
+            <SectionListItem>Error handling</SectionListItem>
+            <SectionListItem>Working with API (fetch, axios)</SectionListItem>
+            <SectionListItem>Promises (asyns, await)</SectionListItem>
+            <SectionListItem>Trello</SectionListItem>
         </SectionList>
     )
 }
