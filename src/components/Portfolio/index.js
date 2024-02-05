705b14cb1ab3382bd65160d117f6bb39af642916
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2024-02-05 23:23:58 +0100

    Portfolio styles updated

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,28 +1,52 @@
 import {
   PorftolioLoading,
+  PortfolioButton,
   PortfolioHeader,
-  PortfolioIcone,
+  PortfolioIcon,
+  PortfolioLoadingSpinner,
   PortfolioProjectDescription,
   PortfolioSubheader,
   PortfolioTile,
+  PortfolioTileElement,
   PortfolioTileHeader,
   PortfolioTileLink,
   PortfolioTileLinks,
+  PortfolioWarning,
+  PortfolioWarningImage,
+  PortfolioWarningText,
+  PortfolioWarningWrapper,
   PortfolioWrapper
 } from "./styled";
 import { useRepositoriumData } from "./useRepositoriumData";
+import SpinnerIcon from "../../image/icon-spinner.png";
+import DangerIcon from "../../image/Danger.png"
 
 export const Portfolio = () => {
   const { status, repositories } = useRepositoriumData();
 
   return (
     <>
-      <PortfolioIcone />
+      <PortfolioIcon />
       <PortfolioHeader>Portfolio</PortfolioHeader>
       <PortfolioSubheader>My recent projects</PortfolioSubheader>
 
-      {status === "loading" && <PorftolioLoading>Please wait, projects are being loaded...</PorftolioLoading>}
-      {status === "error" && <p>Ooops! Something went wrong... </p>}
+      {status === "loading" &&
+        <>
+          <PorftolioLoading>Please wait, projects are being loaded...</PorftolioLoading>
+          <PortfolioLoadingSpinner src={SpinnerIcon} alt="Spiner Icon" />
+        </>
+      }
+      {status === "error" &&
+        <PortfolioWarningWrapper>
+          <PortfolioWarningImage src={DangerIcon} alt="Danger Icon" />
+          <PortfolioWarning>Ooops! Something went wrong... </PortfolioWarning>
+          <PortfolioWarningText>Sorry, failed to load Github projects.
+            You can check them directly on Github.</PortfolioWarningText>
+          <PortfolioButton href="https://github.com/KingaGrelewicz?tab=repositories">
+            Go to GitHub
+          </PortfolioButton>
+        </PortfolioWarningWrapper>
+      }
 
       {status === "success" && (
         <PortfolioWrapper>
@@ -30,16 +54,21 @@ export const Portfolio = () => {
             <PortfolioTile key={repo.id}>
               <PortfolioTileHeader>{repo.name}</PortfolioTileHeader>
               <PortfolioProjectDescription>{repo.description}</PortfolioProjectDescription>
-              <PortfolioTileLinks>Demo:
-                {/* {repo.homepage && ( */}
-                  <PortfolioTileLink href={repo.homepage} target="_blank" rel="noopener noreferrer">
-                {/* )} */}{repo.html_url}
-                </PortfolioTileLink>
+              <PortfolioTileLinks>
+                <PortfolioTileElement>Demo:
+                  {repo.homepage && (
+                    <PortfolioTileLink href={repo.homepage} target="_blank" rel="noopener noreferrer">
+                      {repo.homepage}
+                    </PortfolioTileLink>
+                  )}
+                </PortfolioTileElement>
               </PortfolioTileLinks>
-              <PortfolioTileLinks>Code:
-                <PortfolioTileLink href={repo.html_url} target="_blank" rel="noopener noreferrer">
-                  {repo.html_url}
-                </PortfolioTileLink>
+              <PortfolioTileLinks>
+                <PortfolioTileElement>Code:
+                  <PortfolioTileLink href={repo.html_url} target="_blank" rel="noopener noreferrer">
+                    {repo.html_url}
+                  </PortfolioTileLink>
+                </PortfolioTileElement>
               </PortfolioTileLinks>
             </PortfolioTile>
           ))}
--- src/components/Portfolio/styled.js
+++ src/components/Portfolio/styled.js
@@ -1,14 +1,15 @@
-import styled from "styled-components";
-import { ReactComponent as PortfolioGitHubIcone } from "../../image/Github_black.svg";
+import styled, { keyframes } from "styled-components";
+import { ReactComponent as PortfolioGitHubIcon } from "../../image/Github_black.svg";
 
 export const PortfolioWrapper = styled.div`
     display: grid;
     grid-template-columns: repeat(2, 592px);
     grid-gap: 32px;
     justify-content: center;
+    margin: 72px 0 120px 0;
 `;
 
-export const PortfolioIcone = styled(PortfolioGitHubIcone)`
+export const PortfolioIcon = styled(PortfolioGitHubIcon)`
     display: flex;
     margin: 0 auto;
     fill: ${({ theme }) => theme.color.scienceBlue};
@@ -36,11 +37,65 @@ export const PorftolioLoading = styled.p`
     text-align: center;
 `;
 
+const rotate360 = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+export const PortfolioLoadingSpinner = styled.img`
+    animation: ${rotate360} 5s linear infinite;
+    display: flex;
+    margin: 0 auto;
+`;
+export const PortfolioWarningWrapper = styled.div`
+    display: grid;
+    justify-content: center;
+    justify-items: center;
+    margin: 88px 0 140px 0;
+`;
+export const PortfolioWarningImage = styled.img`
+    width: 37px;
+    height: 34px;
+`;
+
+export const PortfolioWarning = styled.p`
+    font-size: 24px;
+    font-weight: 700;
+    margin: 16px;
+`;
+
+export const PortfolioWarningText = styled.p`
+    font-size: 20px;
+    font-weight: 400;
+    margin: 16px;
+`;
+
+export const PortfolioButton = styled.a`
+text-decoration: none;
+    background: ${({ theme }) => theme.color.scienceBlue};
+    color: ${({ theme }) => theme.color.white};
+    border: 1px solid ${({ theme }) => theme.color.scienceBlue};
+    border-radius: 4%; 
+    padding: 12px 16px;
+    font-size: 20px;
+    font-weight: 600;
+`;
+
 export const PortfolioTile = styled.div`
     background: ${({ theme }) => theme.color.white};
     padding: 16px;
     border: 6px solid ${({theme}) => theme.color.iron};
     border-radius: 4px;
+    box-shadow: 0px 16px 58px 0px rgba(9, 10, 51, 0.03);
+
+    &:hover {
+        border-color: rgba(3, 102, 214, 0.2);
+    }
 `;
 
 export const PortfolioTileHeader = styled.h3`
@@ -58,17 +113,18 @@ export const PortfolioProjectDescription = styled.div`
 `;
 
 export const PortfolioTileLinks = styled.div`
-    display: flex;
-    justify-content: space-between;
+
+`;
+
+export const PortfolioTileLink = styled.a`
+    color: ${({ theme }) => theme.color.scienceBlue};
+    font-size: 18px;
+    padding: 0 5px;
+`;
+
+export const PortfolioTileElement = styled.div`
     color: ${({ theme }) => theme.color.slateGray};
     font-size: 18px;
     font-weight: 400;
     padding: 8px 5px;
-`;
-
-export const PortfolioTileLink = styled.a`
-    display: block;
-    color: ${({ theme }) => theme.color.scienceBlue};
-    font-size: 18px;
-    padding: 0 5px;
 `;
