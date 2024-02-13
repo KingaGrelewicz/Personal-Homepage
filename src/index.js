2ad0da43bd3401503df460dbffa3f534cd8889dc
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2024-02-13 12:25:15 +0100

    ThemeProvider removed from index.js

--- src/index.js
+++ src/index.js
@@ -2,15 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ThemeProvider } from './components/ThemeContex';
-import { light } from './theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ThemeProvider  theme={light}>
     <App />
-    </ThemeProvider>
   </React.StrictMode>
 );
 
