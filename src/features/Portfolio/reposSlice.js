45ebf6426bbf8debc2866bcf0591937863569d34
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2024-03-04 10:29:46 +0100

    reposSlice created

---
+++ src/features/Portfolio/reposSlice.js
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const reposSlice = createSlice({
+    name: 'repos',
+    initialState: {
+        loading: false,
+        error: null,
+        data: [],
+    },
+    fetchReposStart: (state) => {
+        state.loading = true;
+        state.error = null;
+    },
+    fetchReposFailure: (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+    },
+    fetchReposSuccess: (state, { payload: repos }) => {
+        state.loading = false;
+        state.repos = repos;
+    }
+});
+
+export const {
+    fetchReposStart,
+    fetchReposFailure,
+    fetchReposSuccess,
+} = reposSlice.actions;
+
+export const selectReposState = state => state.repos;
+
+export default reposSlice.reducer;
