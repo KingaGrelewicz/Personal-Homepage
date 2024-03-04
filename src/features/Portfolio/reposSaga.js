693b7f8dd7c209e251882de5f5f72df9179d5bac
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2024-03-04 10:29:39 +0100

    reposSaga created

---
+++ src/features/Portfolio/reposSaga.js
@@ -0,0 +1,18 @@
+import { call, delay, put, takeEvery } from "redux-saga/effects";
+import { getRepos } from './getRepos';
+import { fetchRepos, fetchReposFailure, fetchReposStart, fetchReposSuccess } from "./reposSlice";
+
+export function* fetchReposHandler() {
+    try {   
+        yield put(fetchReposStart());
+        yield delay(1000);
+        const reposData = yield call(getRepos);
+        yield put(fetchReposSuccess(reposData));
+    } catch (error) {
+        yield put(fetchReposFailure("ups"));
+    }
+}
+
+export function* watchFetchRepos() {
+    yield takeEvery('repos/fetchRepos', fetchReposHandler);
+}
