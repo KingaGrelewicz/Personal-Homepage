d1ebe7a7c73cecba7ae21fb10c19c2028812607b
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2024-03-09 10:35:27 +0100

    themeLocalStorage updated

--- src/features/themeLocalStorage.js
+++ src/features/themeLocalStorage.js
@@ -1,9 +1,7 @@
 const localStorageKey = "theme";
 
-export const saveThemeInLocalStorage = theme => {
+export const saveThemeInLocalStorage = theme =>
     localStorage.setItem(localStorageKey, JSON.stringify(theme));
-};
 
-export const getThemeFromLocalStorage = () => {
-    return JSON.parse(localStorage.getItem(localStorageKey)) || {};
-};
+export const getThemeFromLocalStorage = () =>
+    JSON.parse(localStorage.getItem(localStorageKey)) || [];
