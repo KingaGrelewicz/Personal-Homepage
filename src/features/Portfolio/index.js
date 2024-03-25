8759e4d385c18901349e864d14073314d6f346fc
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2024-03-25 09:12:00 +0100

    Portfolio component updated

--- src/features/Portfolio/index.js
+++ src/features/Portfolio/index.js
@@ -3,22 +3,17 @@ import {
   PortfolioHeader,
   PortfolioIcon,
   PortfolioSubheader,
-  PortfolioTile,
-  PortfolioTileElement,
-  PortfolioTileHeader,
-  PortfolioTileLink,
-  PortfolioTileLinks,
 } from "./styled";
 import { useEffect } from "react";
-import { selectReposState, setRepos } from "./reposSlice";
+import { setRepos } from "./reposSlice";
 import Loading from "../Loading";
 import Error from "../Error";
+import Tile from "./Tile";
 
 export const Portfolio = () => {
   const dispatch = useDispatch();
   const loading = useSelector(state => state.repos.loading);
   const error = useSelector(state => state.repos.error);
-  const repos = useSelector(selectReposState);
 
   useEffect(() => {
     dispatch(setRepos());
@@ -35,37 +30,7 @@ export const Portfolio = () => {
       {error && <Error />}
 
       {!loading && !error && (
-        <PortfolioWrapper>
-          {repos.map((repo) => (
-            <PortfolioTile key={repo.id}>
-              <PortfolioTileHeader>{_.startCase(repo.name.toLowerCase())}</PortfolioTileHeader>
-              <PortfolioProjectDescription>{repo.description}</PortfolioProjectDescription>
-              <PortfolioTileLinks>
-                <PortfolioTileElement>
-                  Demo:
-                  {repo.homepage && (
-                    <PortfolioTileLink
-                      href={repo.homepage}
-                      target="_blank"
-                      rel="noopener noreferrer">
-                      {repo.homepage}
-                    </PortfolioTileLink>
-                  )}
-                </PortfolioTileElement>
-                <PortfolioTileElement>
-                  Code:
-                  <PortfolioTileLink
-                    href={repo.html_url}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    {repo.html_url}
-                  </PortfolioTileLink>
-                </PortfolioTileElement>
-              </PortfolioTileLinks>
-            </PortfolioTile>
-          ))}
-        </PortfolioWrapper>
+        <Tile />
       )}
     </>
   );
--- src/features/Portfolio/styled.js
+++ src/features/Portfolio/styled.js
@@ -2,19 +2,6 @@ import styled from "styled-components";
 import { ReactComponent as PortfolioGitHubIcon } from "../../image/Github_black.svg";
 import { mediaQuery } from "../../theme";
 
-export const PortfolioWrapper = styled.div`
-    display: grid;
-    grid-template-columns: repeat(2, 592px);
-    grid-gap: 32px;
-    justify-content: center;
-    margin: 72px 0 120px 0;
-
-    @media (max-width: ${mediaQuery.breakpoints.mobile}px) {
-        grid-template-columns: 1fr;
-        margin: 48px 16px;
-    }
-`;
-
 export const PortfolioIcon = styled(PortfolioGitHubIcon)`
     width: 40px;
     height: 40px;
@@ -50,84 +37,5 @@ export const PortfolioSubheader = styled.h3`
     @media (max-width: ${mediaQuery.breakpoints.mobile}px) {
         font-size: 17px;
         margin: 16px 0 24px 0;
-    }
-`;
-
-export const PortfolioTile = styled.div`
-    background: ${({ theme }) => theme.color.secondaryBackground};
-    padding: 56px;
-    border: 6px solid ${({ theme }) => `${theme.color.primaryBorder}33`};
-    border-radius: 4px;
-    display: grid;
-    grid-template-rows: auto 1fr auto;
-    grid-gap: 24px;
-    grid-auto-flow: row;
-    box-shadow: 0px 16px 58px 0px ${({ theme }) => `${theme.color.shadow}08`};
-    transition: border-color 0.3s ease-in-out;
-
-    &:hover {
-        border-color: ${({ theme }) => `${theme.color.mainBlue}33`};
-    }
-
-    @media (max-width: ${mediaQuery.breakpoints.mobile}px) {
-        margin: 0 16px;
-        grid-gap: 16px;
-        padding: 24px;
-    }
-`;
-
-export const PortfolioTileHeader = styled.h3`
-    color: ${({ theme }) => theme.color.mainBlue};
-    font-size: 20px;
-    margin-bottom: 8px;
-
-    @media (max-width: ${mediaQuery.breakpoints.mobile}px) {
-        font-size: 16px;
-    }
-`;
-
-export const PortfolioDescription = styled.div`
-    color: ${({ theme }) => theme.color.primaryTextColor};
-    font-size: 18px;   
-    font-weight: 400;
-    margin-bottom: 16px;
-
-    @media (max-width: ${mediaQuery.breakpoints.mobile}px) {
-        font-size: 14px;
-        display: flex;
-        flex-wrap: wrap;
-    }
-`;
-
-export const PortfolioTileLinks = styled.div``;
-
-export const PortfolioTileLink = styled.a`
-    color: ${({ theme }) => theme.color.mainBlue};
-    font-size: 18px;
-    padding: 0 5px;
-
-    &:hover {
-        filter: brightness(143%);
-    }
-
-    &:active {
-        filter: brightness(156%);
-    }
-
-    @media (max-width: ${mediaQuery.breakpoints.mobile}px) {
-        font-size: 14px;
-    }
-`;
-
-export const PortfolioTileElement = styled.div`
-    display: flex;
-    align-items: center;
-    color: ${({ theme }) => theme.color.secondaryTextColor};
-    font-size: 18px;
-    font-weight: 400;
-    padding: 8px 5px;
-
-    @media (max-width: ${mediaQuery.breakpoints.mobile}px) {
-        font-size: 14px;
     }
 `;
