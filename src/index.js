94840e5d25aa6de5534dd8b06804daef41a5ecb0
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2024-04-04 10:03:07 +0200

    styled normalize added

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import store from './store';
 import { Provider } from 'react-redux';
+import { Normalize } from "styled-normalize";
+import store from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
+    <Normalize />
     <React.StrictMode>
       <App />
     </React.StrictMode>
