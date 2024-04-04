e6a70baa7e25cae237a15d5cbb26fde8a3ddac3a
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2024-04-04 10:07:59 +0200

    Footer updated

--- src/common/Footer/index.js
+++ src/common/Footer/index.js
@@ -1,9 +1,7 @@
+import { email } from "../../email";
+import { SocialIcons } from "./SocialIcons";
 import {
     FooterContent,
-    FooterFacebookIcone,
-    FooterGitHubIcone,
-    FooterIcones,
-    FooterLinkedInIcone,
     FooterMail,
     FooterParagraph,
     FooterWrapper
@@ -14,33 +12,12 @@ const Footer = () => {
     return (
         <FooterWrapper>
             <FooterParagraph>LET'S TALK</FooterParagraph>
-            <FooterMail
-                href="mailto:kinga.grelewicz@gmail.com">kinga.grelewicz@gmail.com
-            </FooterMail>
+            <FooterMail href={`mailto:${email}`}>{email}</FooterMail>
             <FooterContent>
                 I am always open to new challenges.<br />
                 If you need help implementing your ideas, feel free to contact me 👋
             </FooterContent>
-            <FooterIcones>
-                <a 
-                href="https://github.com/KingaGrelewicz" 
-                target="_blank" 
-                rel="noopener noreferrer">
-                    <FooterGitHubIcone />
-                </a>
-                <a 
-                href="https://www.linkedin.com/in/kinga-grelewicz/" 
-                target="_blank" 
-                rel="noopener noreferrer">
-                    <FooterLinkedInIcone />
-                </a>
-                <a 
-                href="https://www.facebook.com/kinga.grelewicz.5/" 
-                target="_blank" 
-                rel="noopener noreferrer">
-                    <FooterFacebookIcone />
-                </a>
-            </FooterIcones>
+            <SocialIcons />
         </FooterWrapper>
     )
 };
--- src/common/Footer/styled.js
+++ src/common/Footer/styled.js
@@ -1,10 +1,7 @@
 import styled from "styled-components";
-import { ReactComponent as GitHubIcone } from "../../image/Github_black.svg";
-import { ReactComponent as LinkedInIcone } from "../../image/LinkedIN_black.svg";
-import { ReactComponent as FacebookIcone } from "../../image/Facebook_black.svg";
 import { mediaQuery } from "../../theme";
 
-export const FooterWrapper = styled.div`
+export const FooterWrapper = styled.footer`
     margin: 24px auto;
     max-width: 1216px;
 
@@ -13,7 +10,7 @@ export const FooterWrapper = styled.div`
     }
 `;
 
-export const FooterParagraph = styled.p`
+export const FooterParagraph = styled.h2`
     color: ${({ theme }) => theme.color.primaryTextColor};
     font-size: 12px;
     font-weight: 700;
@@ -39,7 +36,7 @@ export const FooterMail = styled.a`
     }
 `;
 
-export const FooterContent = styled.div`
+export const FooterContent = styled.p`
     color: ${({ theme }) => theme.color.secondaryTextColor};
     font-size: 18px;
     font-weight: 400;
@@ -48,52 +45,5 @@ export const FooterContent = styled.div`
     @media (max-width: ${mediaQuery.breakpoints.mobile}px) {
         font-size: 14px;
         margin: 12px 0;
-    }
-`;
-
-export const FooterIcones = styled.div`
-    display: grid;
-    grid-template-columns: repeat(3, auto);
-    grid-gap: 24px;
-    width: fit-content;
-    margin-top: 56px;
-
-    @media (max-width: ${mediaQuery.breakpoints.mobile}px) {
-        margin: 40px 0;
-    }
-`;
-
-export const FooterGitHubIcone = styled(GitHubIcone)`
-    fill: ${({ theme }) => theme.color.secondaryTextColor};
-    transition: fill 0.5s;
-
-    &:hover {
-        fill: ${({ theme }) => theme.color.mainBlue};
-    }
-
-    &:active {
-        filter: brightness(150%);
-    }
-`;
-
-export const FooterLinkedInIcone = styled(LinkedInIcone)`
-    fill: ${({ theme }) => theme.color.secondaryTextColor};
-    transition: fill 0.5s;
-
-    &:hover {
-        fill: ${({ theme }) => theme.color.mainBlue};
-    }
-
-    &:active {
-        filter: brightness(150%);
-    }
-`;
-
-export const FooterFacebookIcone = styled(FacebookIcone)`
-    fill: ${({ theme }) => theme.color.secondaryTextColor};
-    transition: fill 0.5s;
-    
-    &:hover {
-        fill: ${({ theme }) => theme.color.mainBlue};
     }
 `;
