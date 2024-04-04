d0b3f8c8d08e9b5fd221d7e519e06c1a1fd4c67c
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2024-04-04 10:05:49 +0200

    getRepos simplified

--- src/features/Portfolio/getRepos.js
+++ src/features/Portfolio/getRepos.js
@@ -1,10 +1,6 @@
 import axios from "axios";
 
 export const getRepos = async () => {
-    try {
       const response = await axios.get('https://api.github.com/users/KingaGrelewicz/repos');
       return response.data;
-    } catch (error) {
-      throw new Error(error.message);
-    }
 };
