0927c8c94c56694cd8959848fa3c39532aaeade8
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2024-09-11 10:47:22 +0200

    Footer component converted to type script

--- src/common/Footer/index.tsx
+++ src/common/Footer/index.tsx
@@ -2,25 +2,25 @@ import React from "react";
 import { email } from "../../email";
 import { SocialIcons } from "./SocialIcons";
 import {
-    FooterContent,
-    FooterMail,
-    FooterParagraph,
-    FooterWrapper
+  FooterContent,
+  FooterMail,
+  FooterParagraph,
+  FooterWrapper,
 } from "./styled";
 
-
-const Footer: React.FC = () => {
-    return (
-        <FooterWrapper>
-            <FooterParagraph>LET'S TALK</FooterParagraph>
-            <FooterMail href={`mailto:${email}`}>{email}</FooterMail>
-            <FooterContent>
-                I am always open to new challenges.<br />
-                If you need help implementing your ideas, feel free to contact me 👋
-            </FooterContent>
-            <SocialIcons />
-        </FooterWrapper>
-    )
+const Footer = () => {
+  return (
+    <FooterWrapper>
+      <FooterParagraph>LET'S TALK</FooterParagraph>
+      <FooterMail href={`mailto:${email}`}>{email}</FooterMail>
+      <FooterContent>
+        I am always open to new challenges.
+        <br />
+        If you need help implementing your ideas, feel free to contact me 👋
+      </FooterContent>
+      <SocialIcons />
+    </FooterWrapper>
+  );
 };
 
 export default Footer;
