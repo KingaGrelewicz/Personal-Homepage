a7ad3fab8647a7ea480a30f10bdba5ec208866cf
Kinga Grelewicz <kinga.grelewicz@gmail.com>
2025-02-10 21:03:48 +0100

    App updated - added: Routers, MenuNav, About, Education, WorkExperience and Skills. FrontendSkilld moved to Porftolio

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,43 @@
-import React from "react";
 import { useSelector } from "react-redux";
+import { HashRouter, Route, Routes } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
 import { Globalstyle } from "./GlobalStyle";
 import Container from "./common/Container";
 import Footer from "./common/Footer";
 import Header from "./common/Header";
 import { Portfolio } from "./features/Portfolio";
-import { Skills } from "./features/Skills";
-import Tools from "./image/tools.png";
-import Rocket from "./image/rocket.png";
-import { ThemeProvider } from "styled-components";
-import {
-  SkillsList,
-  professionalSkills,
-  skillsToLearn,
-} from "./features/SkillsList";
 import { selectIsDarkTheme } from "./common/ThemeToggler/themeSlice";
 import { darkTheme, lightTheme } from "./theme";
+import MenuNav from "./common/MenuNav";
+import About from "./features/About";
+import Education from "./features/Education";
+import WorkExperienceList from "./features/WorkExperienceList";
+import { workExperienceData } from "./features/WorkExperienceList/workExperienceData";
+import Skills from "./features/Skills";
 
 const App: React.FC = () => {
   const isDarkTheme = useSelector(selectIsDarkTheme);
 
   return (
     <ThemeProvider theme={isDarkTheme ? darkTheme : lightTheme}>
-      <Container>
-        <Globalstyle />
-        <Header />
-        <Skills
-          title="My skillset includes"
-          img={Tools}
-          skills={professionalSkills}
-        >
-          <SkillsList items={[]} />
-        </Skills>
-        <Skills
-          title="What I want to learn next"
-          img={Rocket}
-          skills={skillsToLearn}
-        >
-          <SkillsList items={[]} />
-        </Skills>
-        <Portfolio />
-        <Footer />
-      </Container>
+      <HashRouter>
+        <Container>
+          <Globalstyle />
+          <Header />
+          <MenuNav />
+          <Routes>
+            <Route path="/" element={<About />}></Route>
+            <Route
+              path="/experience"
+              element={<WorkExperienceList jobs={workExperienceData} />}
+            ></Route>
+            <Route path="/skills" element={<Skills />}></Route>
+            <Route path="/education" element={<Education />}></Route>
+            <Route path="/frontend" element={<Portfolio />}></Route>
+          </Routes>
+          <Footer />
+        </Container>
+      </HashRouter>
     </ThemeProvider>
   );
 };
